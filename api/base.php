0b0e099d62f0691c7691d14016f90057c8cc5416
ruzs <eddie10913@gmail.com>
2022-12-26 11:29:23 +0800

    完成save()函式,用來新增或更新資料

--- api/base.php
+++ api/base.php
@@ -16,7 +16,6 @@ class DB{
     if(is_array($id)){
       $tmp=$this->arrayToSqlArray($id);
       $sql =$sql. " where " .join(" && ",$tmp);
-
     }else{
       $sql = $sql . " where `id`='$id'";
     }
@@ -28,22 +27,42 @@ class DB{
     if(isset($arg[0])){
       if(is_array($arg[0])){
         $tmp=$this->arrayToSqlArray($arg[0]);
-
         $sql =$sql. " where " .join(" && ",$tmp);
-
       }else{
         $sql = $sql . $arg[0];
       }
     }
-
     if(isset($arg[1])){
       $sql = $sql . $arg[1];
     }
     return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
   }
-  public function save(){}
-  public function del(){}
+  public function save($array){
+    if (isset($array['id'])){
+      //更新
+      $id=$array['id'];
+      unset($array['id']);
+      $tmp=$this->arrayToSqlArray($array);
+      $sql="update $this->table set ".join(",",$tmp)." where `id`='$id'";
+    }else{
+      //新增
+      $cols=array_keys($array);
+      $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) values('".join("','",$array) ."' )";
+    }
+    echo $sql;
+    $this->pdo->exec($sql);
+  }
+  public function del($id){
+    $sql="delete from $this->table ";
+    if(is_array($id)){
+      $tmp=$this->arrayToSqlArray($id);
+      $sql = $sql . " where " .join(" && ",$tmp);
+    }else{
+      $sql = $sql . " where `id`='$id'";
+    }
+    return $this->pdo->exec($sql);
+  }
   public function sum(){}
   public function count(){}
   public function max(){}
@@ -56,17 +75,20 @@ class DB{
     return $tmp;
   }
 }
-
-
-function dd(){
-
+function dd($array){
+  echo "<pre>";
+  print_r($array);
+  echo "</pre>";
 }
-function to(){
-
+function to($location){
+  header("location:$location");
 }
-function q(){
-
+function q($sql){
+  global $pdo;
+  echo $sql;
+  return $pdo->query($sql)->fetchAll();
 }
+
 $db=new DB('bottom');
 $bot=$db->find(1);
 print_r($bot);
@@ -74,5 +96,17 @@ echo "<hr>";
 $db=new DB('bottom');
 $bot=$db->all();
 print_r($bot);
+echo "<hr>";
+// $db=new DB('bottom');
+// $bot=$db->del(2);
+// print_r($bot);
+// print_r($db->all());
+// echo "<hr>";
+$row=$db->find(3);
+print_r($row);
+
+$row['bottom']="bbb";
+print_r($row);
+$db->save($row);
 echo "<hr>";
 ?>
