e9c8762e992a0bb5012e2ad540e978d03c2a5d0f
ruzs <eddie10913@gmail.com>
2022-12-29 10:28:12 +0800

    在單選項目上增加判斷是否為勾選狀態

--- back/title.php
+++ back/title.php
@@ -13,6 +13,7 @@
         <?php
           $rows=$Title->all();
           foreach($rows as $row){
+            $checked=($row['sh']==1)?"checked":"";
         ?>
         <tr>
           <td width="45%">
@@ -22,7 +23,7 @@
             <input type="text" name="text[]" value="<?=$row['text']?>">
           </td>
           <td width="7%">
-            <input type="radio" name="sh" value="<?=$row['id']?>">
+            <input type="radio" name="sh" value="<?=$row['id']?>"> <?=$checked;?>>
           </td>
           <td width="7%">
             <input type="checkbox" name="del" value="<?=$row['id']?>">
