4753b290953ac647a82579e6f79af16b949b8df8
ruzs <eddie10913@gmail.com>
2022-12-30 15:11:20 +0800

    在前台顯示動畫輪播圖片

--- front/home.php
+++ front/home.php
@@ -11,24 +11,34 @@
   </marquee>
   <div style="height:32px; display:block;"></div>
   <!--正中央-->
-  <script>
-    var lin = new Array();
-    var now = 0;
-    if (lin.length > 1) {
-      setInterval("ww()", 3000);
-      now = 1;
-    }
-    function ww() {
-      $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
-      //$("#mwww").attr("src",lin[now])
-      now++;
-      if (now >= lin.length)
-        now = 0;
-    }
-  </script>
   <div style="width:100%; padding:2px; height:290px;">
     <div id="mwww" loop="true" style="width:100%; height:100%;">
-      <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+      <!-- <div style="width:99%; height:100%; position:relative;" class="cent">
+        沒有資料
+      </div> -->
+      <script>
+        var lin = new Array();
+        <?php
+          $mvs=$Mvim->all(['sh'=>1]);
+          foreach ($mvs as $mv) {
+            echo "lin.push('./upload/{$mv['img']}');";
+          }
+        ?>
+        var now = 0;
+        ww();
+        if (lin.length > 1) {
+          setInterval("ww()", 3000);
+          now = 1;
+        }
+        
+        function ww() {
+          $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+          //$("#mwww").attr("src",lin[now])
+          now++;
+          if (now >= lin.length)
+            now = 0;
+        }
+      </script>
     </div>
   </div>
   <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
