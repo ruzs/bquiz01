bb6eb6681cde699eb3b631dd5bdb4d19362234d4
ruzs <eddie10913@gmail.com>
2022-12-30 13:11:36 +0800

    完成後台總人數及頁尾版權管理功能

---
+++ api/update.php
@@ -0,0 +1,8 @@
+<?php
+  include_once "base.php";
+  $table=$_POST['table'];
+  $row=$$table->find(1);
+  $row[lcfirst($table)]=$_POST[lcfirst($table)];
+  $$table->save($row);
+  to("../back.php?do=".lcfirst($table));
+?>
