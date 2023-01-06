67b5c24147b03a6f4989e53932776ab0f8538dea
ruzs <eddie10913@gmail.com>
2023-01-06 12:00:41 +0800

    完成管理登入功能

---
+++ api/login.php
@@ -0,0 +1,10 @@
+<?php
+  include_once "base.php";
+
+  $_POST['acc'];
+  if($Admin->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']])>0){
+    to("../back.php");
+  }else{
+    to("../index.php?do=login&error=帳號或密碼錯誤");
+  }
+?>
